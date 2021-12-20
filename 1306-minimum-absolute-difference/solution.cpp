93fb67a0163ee2581640dcb8f0d74478ec14a473
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2021-12-20 06:37:05 +0000

    [LeetCode Sync] Runtime - 68 ms (5.14%), Memory - 32.2 MB (100.00%)

---
+++ 1306-minimum-absolute-difference/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> answer;
+        int minPairDiff = INT_MAX;
+        for (int i = 0; i < arr.size() - 1; ++i) {            
+            int currPairDiff = arr[i + 1] - arr[i];
+            if (currPairDiff == minPairDiff) {
+                answer.push_back({arr[i], arr[i + 1]});           
+            } else if (currPairDiff < minPairDiff) {
+                answer = {};
+                answer.push_back({arr[i], arr[i + 1]});
+                minPairDiff = currPairDiff;
+            }
+        }
+        return answer;
+    }
+};
+
