08f4ad7e84a26dd4adbff5c6d9922ffe87f6890d
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2022-03-19 15:14:39 +0000

    [LeetCode Sync] Runtime - 300 ms (5.02%), Memory - 81.2 MB (87.23%)

---
+++ 1464-reduce-array-size-to-the-half/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> map;
+        for(auto &i:arr){
+            if(map.find(i)==map.end()){
+                map[i]=1;
+            } else {
+                map[i]++;
+            }
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>> pq;
+        for(auto& i:map){
+            pq.push(make_pair(i.second,i.first));
+        }
+        int ans=arr.size();
+        int n=arr.size()/2;
+        int x=0;
+        while(ans>n){
+            auto i=pq.top();
+            pq.pop();
+            ans-=i.first;
+            x++;
+        }
+        return x;
+    }
+};
