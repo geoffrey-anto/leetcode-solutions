f43a0b1127889608c3126b6d057b10c854ecc091
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2022-03-22 19:15:13 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

---
+++ problems/move_zeroes/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int prev=0;
+        for(int &i:nums){
+            if(i!=0){
+                swap(nums[prev],i);
+                prev++;
+            }
+        }
+    }
+};
