96f87104a279ab32b8ca2f453d09a5165d778895
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2022-03-25 06:56:17 +0000

    [LeetCode Sync] Runtime - 3 ms (69.08%), Memory - 7.8 MB (100.00%)

---
+++ 1095-two-city-scheduling/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    static bool cmpr(vector<int> &a, vector<int> &b){
+        return (a[0]-a[1])<(b[0]-b[1]);
+    }
+    
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), cmpr);
+        int c=0;
+        int n=costs.size();
+        for(int i=0;i<n/2;i++){
+            c+=costs[i][0];
+        }
+        for(int i=n/2;i<n;i++){
+            c+=costs[i][1];
+        }
+        return c;
+    }
+};
