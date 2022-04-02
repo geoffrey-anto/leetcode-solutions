62500046e38a53c664ccd0537e2cebb342011d7f
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2022-04-02 06:17:25 +0000

    [LeetCode Sync] Runtime - 6 ms (5.43%), Memory - 6.6 MB (100.00%)

---
+++ 0728-self-dividing-numbers/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i=left;i<right+1;i++){
+            int num = i;
+            bool flg=true;
+            while(num!=0){
+                if(num%10!=0){
+                    if((i%(num%10))==0){
+                        num/=10;
+                        continue;
+                    } else {
+                        flg = false;
+                        num/=10;
+                        break;
+                    }
+                } else {
+                    flg=false;
+                    break;
+                }
+                num/=10;
+            }
+            if(flg){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
