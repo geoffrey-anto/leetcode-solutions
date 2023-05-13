7dff1e5bf4ff623c097b8927f6be33d35ee57af9
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-05-13 18:05:48 +0000

    [LeetCode Sync] Runtime - 9 ms (51.72%), Memory - 7.4 MB (100.00%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int arr[27];
+        memset(arr, 0, sizeof(arr));
+        for(auto &i:s) {
+            arr[i-'a']++;
+        }
+        for(auto &i:t) {
+            arr[i-'a']--;
+        }
+
+        for(auto &i:arr) {
+            if(i!=0) {
+                return false;
+            }
+        }
+        return true;
+     }
+};
