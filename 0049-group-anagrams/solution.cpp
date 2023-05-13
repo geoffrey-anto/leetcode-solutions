e2cbe2eb54ae5e3d7c0ff31247fece381f30dcbc
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-05-13 18:26:57 +0000

    [LeetCode Sync] Runtime - 27 ms (62.79%), Memory - 20.7 MB (99.78%)

---
+++ 0049-group-anagrams/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+
+        unordered_map<string, vector<string>> map;
+
+        for(int i=0; i<n; i++) {
+            auto s = strs[i];
+            sort(s.begin(), s.end());
+
+            map[s].push_back(strs[i]);
+        }
+
+        vector<vector<string>> v;
+
+        for(auto i: map) {
+            v.push_back(i.second);
+        }
+        return v;
+    }
+};
