e2420a16eb5ad76342e8845ac17578b7b0decc6e
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-05-14 09:05:27 +0000

    [LeetCode Sync] Runtime - 20 ms (7.74%), Memory - 13.6 MB (99.94%)

---
+++ 0347-top-k-frequent-elements/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+
+        for(auto &i: nums) {
+            mp[i]++;
+        }
+
+        priority_queue<pair<int,int>> pq;
+
+        for(auto &i: mp) {
+            pq.push({i.second, i.first});
+        }
+
+        vector<int> ans;
+
+        for(int i=0; i<k; i++) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
