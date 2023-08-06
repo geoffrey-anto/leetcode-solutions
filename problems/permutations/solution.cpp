19317fa9a0fb9b94a565cd796b6ef7712cebc099
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-08-06 11:37:14 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void f(vector<int> &n, int idx, vector<vector<int>> &ans){
-        if(idx==n.size()){
+        if(idx == n.size()) {
             ans.push_back(n);
             return;
         }
-        for(int i=idx;i<n.size();i++){
-            swap(n[idx],n[i]);
-            f(n,idx+1,ans);
-            swap(n[idx],n[i]);
+
+        for(int i=idx; i<n.size(); i++) {
+            swap(n[i], n[idx]);
+            f(n, idx+1, ans);
+            swap(n[i], n[idx]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
