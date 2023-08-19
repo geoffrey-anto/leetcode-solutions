9c46d5cddbe876ddeea2dd9dc457dea20c46a674
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-08-19 14:42:07 +0000

    Sync LeetCode submission - Make String a Subsequence Using Cyclic Increments (cpp)

---
+++ problems/make_string_a_subsequence_using_cyclic_increments/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    char getNextChar(char a) {
+        int ascii = int(a);
+        if(ascii == 122) {
+            return 'a';
+        }
+        return char(ascii+1);
+    }
+    
+    bool canMakeSubsequence(string str1, string str2) {
+        if(str2.length() > str1.length()) {
+            return false;
+        }
+        
+        int cnt = 0;
+        
+        for(auto i: str1) {
+            if(getNextChar(i) == str2[cnt] || i==str2[cnt]) {
+                cnt++;
+            }
+        }
+        
+        if(cnt == str2.length()) return true;
+        return false;
+    }
+};
