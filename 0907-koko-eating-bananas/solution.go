3dd4415b82c5346a4d52abda77928ba8c93b216b
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-10 19:17:03 +0000

    [LeetCode Sync] Runtime - 28 ms (13.52%), Memory - 7.3 MB (5.69%)

---
+++ 0907-koko-eating-bananas/solution.go
@@ -0,0 +1,27 @@
+func minEatingSpeed(piles []int, h int) int {
+    left, right := 1, 0
+    
+    for i, e := range piles {
+     if i==0 || e > right {
+         right = e
+     }
+    }
+
+
+    for left < right {
+        mid := (left + right) / 2
+        hours := 0
+        
+        for _, pile := range(piles) {
+            hours += int(math.Ceil(float64(pile) / float64(mid)))
+        }
+        
+        if hours <= h {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return right
+
+}
