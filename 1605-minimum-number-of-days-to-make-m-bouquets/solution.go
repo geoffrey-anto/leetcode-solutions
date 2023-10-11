418320bb78962d2bf7fb7dcf8bb6c6eaae6259e4
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-11 12:51:38 +0000

    [LeetCode Sync] Runtime - 144 ms (7.55%), Memory - 11.7 MB (5.66%)

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.go
@@ -0,0 +1,54 @@
+func isPossible(bloomDay []int, m int, k int, idx int) bool {
+    cnt := 0
+
+    for _, v := range bloomDay {
+        if v <= idx {
+            cnt++;
+
+            if cnt == k {
+                cnt = 0;
+                m--;
+            }
+        } else {
+            cnt = 0;
+        }
+    }
+
+    return m<=0;
+}
+
+func minDays(bloomDay []int, m int, k int) int {
+    n := len(bloomDay)
+
+    if m * k > n {
+        return -1
+    } 
+
+    min := 1000_000_001
+    max := 0
+
+    for _, v := range bloomDay {
+        if v < min {
+            min = v
+        }
+
+        if v > max {
+            max = v
+        }
+    }
+
+    left, right := min, max
+    
+    for left <= right {
+        x := (left + right) / 2
+
+        if (isPossible(bloomDay, m, k, x)) {
+            right = x - 1;
+        } else {
+            left = x + 1;
+        }
+
+    }
+
+    return left
+}
