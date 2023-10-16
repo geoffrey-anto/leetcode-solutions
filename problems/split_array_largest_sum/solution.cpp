95f7fe1c9f9c3d849141b59e1b82cf4602b771ca
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-16 18:05:32 +0000

    Sync LeetCode submission - Split Array Largest Sum (cpp)

---
+++ problems/split_array_largest_sum/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool isPossible(vector<int>& arr, int n, int k, int curMin) {
+        int res = 1;
+        int curSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > curMin) {
+                return false;
+            }
+
+            if (curSum + arr[i] > curMin) {
+                res += 1;
+
+                curSum = arr[i];
+
+                if (res > k) {
+                    return false;
+                }
+            }
+            else {
+                curSum += arr[i];
+            }
+        }
+        return true;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int l = 0;
+        int r = 0;
+
+        for(int i=0;i<nums.size();++i) l=max(l,nums[i]), r+=nums[i];
+
+
+        while (l<r) {
+            int m = (l+r)/2;
+
+            if (isPossible(nums, nums.size(), k, m)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    }
+};
