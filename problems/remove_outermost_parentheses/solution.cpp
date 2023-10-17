e0d2e333dea75942486d60211fc9f09745f1e43d
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-17 17:29:26 +0000

    Sync LeetCode submission - Remove Outermost Parentheses (cpp)

--- problems/remove_outermost_parentheses/solution.cpp
+++ problems/remove_outermost_parentheses/solution.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    string removeOuterParentheses(string S) {
-        string ans;
-        int o = 0;
-        for (char c : S) {
-            if (c == '(' && o++ > 0) ans += c;
-            if (c == ')' && o-- > 1) ans += c;
+    string removeOuterParentheses(string s) {
+        string ans = "";
+
+        int no = 0;
+
+        for(auto &i :s) {
+            if (i == '(') {
+                if(no > 0) {
+                    ans += i;
+                }
+                no++;
+            } else {
+                if ( no > 1 ) {
+                    ans += i;
+                }
+                no--;
+            }
         }
         return ans;
     }
