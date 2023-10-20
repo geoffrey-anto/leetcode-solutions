5691033a31278b83d3ff17a4722d5c85366e0480
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-20 20:38:24 +0000

    Sync LeetCode submission - Sum of Beauty of All Substrings (cpp)

---
+++ problems/sum_of_beauty_of_all_substrings/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxAndMin(unordered_map<char, int> &mp){
+        int mx = 0, mn = INT_MAX;
+        for (auto it : mp) {
+            mx = max(mx, it.second);
+            mn = min(mn, it.second);
+        }
+        return {mx, mn};      
+    }
+    int beautySum(string s) {
+        int sum = 0;
+        int n = s.length();        
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp.clear();
+            for (int j = i; j < n; j++) {
+                mp[s[j]]++;
+                auto x = maxAndMin(mp);                
+
+                sum += (x[0] - x[1]);
+            }
+        }
+        return sum;
+    }
+};
