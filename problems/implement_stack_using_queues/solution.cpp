a627c7369f387ce208bf3245af3c71f12422978a
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-10-21 14:25:55 +0000

    Sync LeetCode submission - Implement Stack using Queues (cpp)

---
+++ problems/implement_stack_using_queues/solution.cpp
@@ -0,0 +1,81 @@
+// class MyStack {
+// public:
+//     queue<int> q1;
+//     queue<int> q2;
+//     int t;
+//     MyStack() {
+//     }
+    
+//     void push(int x) {
+//         q1.push(x);
+//         t = x;
+//     }
+    
+//     int pop() {
+//         // while q1 has more than 1 elem
+//         while (q1.size() > 1) {
+//             t = q1.front();
+//             q1.pop();
+//             q2.push(t);
+//         }
+
+//         int val = q1.front();
+
+//         q1.pop();
+
+//         swap(q1, q2);
+
+//         return val;
+//     }
+    
+//     int top() {
+//         return t;
+//     }
+    
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
+
+class MyStack {
+public:
+    deque<int> q1;
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q1.push_back(x);
+    }
+    
+    int pop() {
+        int val = q1.back();
+        q1.pop_back();
+        return val;
+    }
+    
+    int top() {
+        return q1.back();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
