054b946fb098b8da7f608ac77729ab73e2360651
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-15 09:56:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void f(vector<vector<int>> &ans, vector<int> temp, int idx, vector<int> nums){
-            ans.push_back(temp);
+    void solve(vector<int> &nums, int idx, vector<int> &ds, vector<vector<int>> &ans, int n) {
+        ans.push_back(ds);
 
-        for(int i=idx; i<nums.size(); i++) {
-            if(idx!=i && nums[i-1]==nums[i]) continue;
+        for(auto i=idx; i<n; i++) {
+            if(i > idx && nums[i] == nums[i-1]) {
+                continue;
+            }
 
-            temp.push_back(nums[i]);
-            f(ans, temp, i+1, nums);
-            temp.pop_back();
+            ds.push_back(nums[i]);
+            solve(nums, i+1, ds, ans, n);
+            ds.pop_back();
         }
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        vector<int> temp;
+        vector<int> ds;
         sort(nums.begin(), nums.end());
-        f(ans, temp, 0, nums);
+        solve(nums, 0, ds, ans, n);
         return ans;
     }
 };
