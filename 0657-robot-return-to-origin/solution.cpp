9590f223814d9b5348f556fcfd25f76e6121e4bb
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-16 13:13:34 +0000

    [LeetCode Sync] Runtime - 11 ms (43.75%), Memory - 8.3 MB (100.00%)

---
+++ 0657-robot-return-to-origin/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto &i: moves) {
+            if(i == 'U') {
+                y++;
+            } else if(i == 'D') {
+                y--;
+            } else if(i == 'R') {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        return (x == 0) and (y == 0);
+    }
+};
