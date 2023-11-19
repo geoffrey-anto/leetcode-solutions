c4a9451cb0d32215d33cb3243dc5c9b519fb7f47
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-19 19:53:38 +0000

    [LeetCode Sync] Runtime - 2 ms (59.74%), Memory - 8.2 MB (100.00%)

---
+++ 0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int f(int x, int y, int m, int n, vector<vector<int>>& nums,vector<vector<int>> &dp) {
+        if(x == m-1 and y == n-1) {
+            if (nums[x][y] == 1){
+                return 0;
+            }
+            return 1;
+        }
+
+        if(x >= m  or y >= n) {
+            return 0;
+        }
+
+        if(nums[x][y] == 1) return 0;
+        
+        if(dp[x][y] != -1) return dp[x][y];
+
+        return dp[x][y] = f(x+1, y, m, n, nums,dp) + f(x, y+1, m, n, nums, dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(0, 0, m, n, obstacleGrid,dp);
+    }
+};
