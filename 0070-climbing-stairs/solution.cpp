7f2c3cb1fa49084e54c8bf254c59d1b51ccb98e3
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-19 11:46:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.6 MB (100.00%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    
-    int dp[100];
-    
-    int f(int i){
-        if(i==0) return dp[i] = 1;
-        if(i==1) return dp[i] = 1;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = f(i-1) + f(i-2);
+    int f(int n, vector<int> &dp) {
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = f(n-1, dp) + f(n-2, dp); 
     }
-    
     int climbStairs(int n) {
-        memset(dp, -1, sizeof(dp));
-        return f(n);
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
     }
 };
