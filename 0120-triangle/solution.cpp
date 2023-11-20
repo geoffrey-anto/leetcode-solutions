80a2e930e297b0fae2d655462ed7858d1c1e1be9
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-20 06:44:08 +0000

    [LeetCode Sync] Runtime - 6 ms (34.92%), Memory - 9.3 MB (100.00%)

--- 0120-triangle/solution.cpp
+++ 0120-triangle/solution.cpp
@@ -1,79 +1,25 @@
-// Memo
-// class Solution {
-// public:
-    
-//     int f(int i,int j, vector<vector<int>>& arr, int n, vector<vector<int>>& dp){
-//         if(i==n-1){
-//             return arr[n-1][j];
-//         }
-        
-//         if(dp[i][j] != -1) return dp[i][j];
-        
-//         int d = arr[i][j] + f(i+1, j , arr, n, dp);
-//         int dg = arr[i][j] + f(i+1, j+1, arr, n, dp);
-        
-//         return dp[i][j] = min(d,dg);
-//     }
-    
-//     int minimumTotal(vector<vector<int>>& triangle) {
-//         int n = triangle.size();
-//         vector<vector<int>> dp(n, vector<int>(n, -1));
-//         return f(0, 0, triangle, n, dp);
-//     }
-// };
-
-// Tabulation
-
-// class Solution {
-// public:
-    
-//     int f(int i,int j, vector<vector<int>>& arr, int n, vector<vector<int>>& dp){
-//         if(i==n-1){
-//             return arr[n-1][j];
-//         }
-        
-//         if(dp[i][j] != -1) return dp[i][j];
-        
-//         int d = arr[i][j] + f(i+1, j , arr, n, dp);
-//         int dg = arr[i][j] + f(i+1, j+1, arr, n, dp);
-        
-//         return dp[i][j] = min(d,dg);
-//     }
-    
-//     int minimumTotal(vector<vector<int>>& triangle) {
-//         int n = triangle.size();
-//         vector<vector<int>> dp(n, vector<int>(n, 0));
-//         for(int i=0; i<n;i++) dp[n-1][i] = triangle[n-1][i];
-//         for(int i=n-2; i>=0; i--){
-//             for(int j=i; j>=0; j--){
-//                 int d = triangle[i][j] + dp[i+1][j];
-//                 int dg = triangle[i][j] + dp[i+1][j+1];
-                
-//                 dp[i][j] = min(d, dg);
-//             }
-//         }
-//         return dp[0][0];
-//     }
-// };
-
-
-
 class Solution {
-public:    
+public:
+    int solve(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+        if(i == n-1) {
+            return triangle[i][j];
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int a = solve(i+1, j, n, triangle, dp) + triangle[i][j];
+        int b = solve(i+1, j+1, n, triangle, dp) + triangle[i][j];
+
+        return dp[i][j] = min(a, b);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> prev(n, 0);
-        for(int i=0; i<n;i++) prev[i] = triangle[n-1][i];
-        for(int i=n-2; i>=0; i--){
-            vector<int> curr(n, 0);
-            for(int j=i; j>=0; j--){
-                int d = triangle[i][j] + prev[j];
-                int dg = triangle[i][j] + prev[j+1];
-                
-                curr[j] = min(d, dg);
-            }
-            prev=curr;
-        }
-        return prev[0];
+
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return solve(0, 0, n, triangle, dp);
     }
 };
