4b10589bfe6622c39c33cdd8580791829c64b816
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-11-27 19:44:08 +0000

    Sync LeetCode submission - Special Positions in a Binary Matrix (cpp)

---
+++ problems/special_positions_in_a_binary_matrix/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int cnt = 0;
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<int> r(m, 0);
+        vector<int> c(n, 0);
+        
+        for (auto i = 0; i < m; i++) {
+            for (auto j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    r[i]++;
+                    c[j]++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 and r[i] == 1 and c[j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
