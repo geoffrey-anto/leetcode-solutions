8a12415eb3565d4e1f15ddb0c132fdeb9c65d9a2
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-05 17:47:49 +0000

    [LeetCode Sync] Runtime - 3 ms (42.88%), Memory - 6.3 MB (100.00%)

--- 1806-count-of-matches-in-tournament/solution.cpp
+++ 1806-count-of-matches-in-tournament/solution.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        if(n <= 1) {
-            return 0;
-        }
-        
-        if(n%2 == 0) {
-            return (n/2) + numberOfMatches(n/2);
-        } else {
-            return ((n-1) / 2) + numberOfMatches((n-1) / 2 + 1);
-        }
+        if(n<=1) return 0;
+        return (n%2==0) ? (n/2) + numberOfMatches(n/2) : (n - 1) / 2 + numberOfMatches((n - 1) / 2 + 1) ;
     }
 };
