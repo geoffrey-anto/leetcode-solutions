4e65fa8227f6dfc2b9bd3e5a857579af0f4ccac2
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-08 18:03:32 +0000

    Sync LeetCode submission - Construct String from Binary Tree (cpp)

---
+++ problems/construct_string_from_binary_tree/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void preorderTraversal(TreeNode* node, string& ans) {
+        if (node == nullptr) {
+            return;
+        }
+        ans += to_string(node->val);
+        if (node->left != nullptr || node->right != nullptr) {
+            ans += "(";
+            preorderTraversal(node->left, ans);
+            ans += ")";
+        }
+        if (node->right != nullptr) {
+            ans += "(";
+            preorderTraversal(node->right, ans);
+            ans += ")";
+        }
+
+    }
+    string tree2str(TreeNode* root) {
+        string ans = "";
+        preorderTraversal(root, ans);
+        return ans;
+    }
+    
+};
