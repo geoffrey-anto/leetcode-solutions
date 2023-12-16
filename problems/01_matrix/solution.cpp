7e8f275e5eb1649b6a68b52c106b7d940d6d6bff
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-16 18:49:56 +0000

    Sync LeetCode submission - 01 Matrix (cpp)

---
+++ problems/01_matrix/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> d = {0, 1, 0, -1, 0};
+    bool isValid(int i, int j, int n, int m) {
+        return i>=0 && i<n && j>=0 && j < m;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        queue<pair<int, int>> q;
+        
+        for(int i=0; i<n; i++) {
+            for(int  j=0; j<m; j++) {
+                if(mat[i][j] == 0) {
+                    q.push(make_pair(i, j));
+                } else {
+                    mat[i][j] = INT_MIN;
+                }
+            }
+        }
+
+        while(!q.empty()) {
+            auto top = q.front();
+            q.pop();
+            int l = top.first;
+            int r = top.second;
+
+            for(int i=0; i<4; i++) {
+                int dy = l + d[i];
+                int dx = r + d[i+1];
+
+                if(isValid(dy, dx, n, m) && mat[dy][dx] == INT_MIN) {
+                    mat[dy][dx] = mat[l][r] + 1;
+                    q.push(make_pair(dy, dx));
+                }
+            }
+        }
+
+        return mat;
+    }
+};
