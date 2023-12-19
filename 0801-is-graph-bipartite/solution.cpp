657c25d39c80ce6c0a88750c82e4388292f39a16
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-19 14:33:51 +0000

    [LeetCode Sync] Runtime - 19 ms (46.68%), Memory - 13.9 MB (100.00%)

---
+++ 0801-is-graph-bipartite/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>> &graph, vector<int> &colors, int c) {
+        colors[node] = c;
+
+        for(auto n: graph[node]) {
+            if(colors[n] == -1) {
+                if(dfs(n, graph, colors, !c) == false) return false; 
+            } else {
+                if(colors[n] == colors[node]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> colors(n+1, -1);
+
+        for(int i=0; i<n; i++) {
+            if(colors[i] == -1) {
+                if(!dfs(i, graph, colors, 0)) return false;
+            }
+        }
+
+        return true;
+    }
+};
