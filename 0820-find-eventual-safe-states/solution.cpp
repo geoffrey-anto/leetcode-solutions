f9b610a6a5b7f7f3c900c2f00b07a6dbd7d1bdb0
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-22 09:19:50 +0000

    [LeetCode Sync] Runtime - 165 ms (12.87%), Memory - 62 MB (60.37%)

---
+++ 0820-find-eventual-safe-states/solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<vector<int>> adj(v, vector<int>());
+        vector<int> inDeg(v, 0);
+        vector<int> ans;
+        for(auto i=0; i<v; i++) {
+            for(auto it: graph[i]) {
+                adj[it].push_back(i);
+                inDeg[i]++;
+            }
+        }
+
+        graph.clear();
+
+        queue<int> q;
+
+        for(int i=0; i<v; i++) {
+            if(inDeg[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int top = q.front();
+            q.pop();
+
+            ans.push_back(top);
+
+            for(auto i: adj[top]) {
+                inDeg[i]--;
+
+                if(inDeg[i] == 0) {
+                    q.push(i);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
