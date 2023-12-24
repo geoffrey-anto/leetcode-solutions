0dd8f9069b78ce0d4f56bc6908169eaca3d270af
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-24 14:37:10 +0000

    Sync LeetCode submission - Minimum Changes To Make Alternating Binary String (cpp)

---
+++ problems/minimum_changes_to_make_alternating_binary_string/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        bool isOne = false;
+        int cnt1 = 0;
+        // 0 start
+        for(int i=0; i<n; i++) {
+            if(isOne) {
+                if(s[i] == '1') {
+                    cnt1++;
+                }
+            } else {
+                if(s[i] == '0') {
+                    cnt1++;
+                }
+            }
+            isOne = !isOne;
+        }
+        // 1 start
+        isOne = true;
+        int cnt2 = 0;
+        for(int i=0; i<n; i++) {
+            if(isOne) {
+                if(s[i] == '1') {
+                    cnt2++;
+                }
+            } else {
+                if(s[i] == '0') {
+                    cnt2++;
+                }
+            }
+            isOne = !isOne;
+        }
+
+        return min(cnt1, cnt2);
+    }
+};
