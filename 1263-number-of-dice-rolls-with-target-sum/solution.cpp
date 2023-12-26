c25d21e93d85fad8023f5c834a0c303426338354
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-26 08:41:08 +0000

    [LeetCode Sync] Runtime - 11 ms (85.62%), Memory - 18.3 MB (5.90%)

---
+++ 1263-number-of-dice-rolls-with-target-sum/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    long long MOD = 1e9 + 7;
+public:
+    int f(int n, int k, int target, vector<vector<int>> &dp) {
+        if (n == 0 or target <= 0) {
+            if(target == 0 && n == 0) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        int res = 0;
+
+        if(dp[n][target] != -1) {
+            return dp[n][target];
+        }
+
+        for (auto i = 1; i <= k; i++) {
+            res = (res + f(n - 1, k, target - i, dp)) % MOD;
+        }
+
+        return dp[n][target] = res;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(31, vector<int>(1001, -1));
+        return f(n, k, target, dp);
+    }
+};
