0af92c4d3accb654fb632e48691b987bbe292d69
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2023-12-28 19:03:28 +0000

    [LeetCode Sync] Runtime - 327 ms (5.40%), Memory - 56.7 MB (18.59%)

---
+++ 1442-number-of-operations-to-make-network-connected/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>> &g, vector<int> &vis) {
+        vis[node] = 1;
+        for(auto n: g[node]) {
+            if(!vis[n]) {
+                dfs(n, g, vis);
+            }
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int m = connections.size();
+
+        if(m < n-1) {
+            return -1;
+        }
+
+        int noOfComponents = 0;
+        vector<vector<int>> g(n, vector<int>());
+        vector<int> vis(n, 0);
+
+        for(auto i: connections) {
+            g[i[0]].push_back(i[1]);
+            g[i[1]].push_back(i[0]);
+        }
+
+        // connections.clear();
+
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                dfs(i, g, vis);
+                noOfComponents++;
+            }
+        }
+
+        return noOfComponents - 1;
+    }
+};
