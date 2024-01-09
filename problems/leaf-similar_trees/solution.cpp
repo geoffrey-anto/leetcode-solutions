6a6413078ee164f4f3134a65153a3847bb68d5f1
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-01-09 10:58:49 +0000

    Sync LeetCode submission - Leaf-Similar Trees (cpp)

---
+++ problems/leaf-similar_trees/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void f(TreeNode* node, vector<int>& v) {
+        if (node == NULL) return;
+
+        if (node->left == NULL && node->right == NULL) {
+            v.push_back(node->val);
+        }
+
+        f(node->left, v);
+        f(node->right, v);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1;
+        vector<int> v2;
+
+        f(root1, v1);
+        f(root2, v2);
+
+        return v1 == v2;
+    }
+
+};
