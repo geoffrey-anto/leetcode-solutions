15bc08bafce205f8737792ccbdb94fffab935145
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-01-14 21:06:30 +0000

    Sync LeetCode submission - Determine if Two Strings Are Close (cpp)

--- problems/determine_if_two_strings_are_close/solution.cpp
+++ problems/determine_if_two_strings_are_close/solution.cpp
@@ -1,36 +1,23 @@
+
+
+
+        
+    
 class Solution {
-public:
-    bool closeStrings(string word1, string word2) {
-        if(word1.length() != word2.length()) return false;
-
-        unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
-
-        for(auto &i: word1) {
-            mp1[i]++;
-        }
-        for(auto &i: word2) {
-            mp2[i]++;
-        }
-
-        for(auto &i:mp1) {
-            bool found = false;
-            bool found2 = false;
-            for(auto &j: mp2) {
-                if(i.first == j.first) {
-                    found = true;
-                }
-                if(i.second == j.second && (i.second!=-1 && j.second!=-1)) {
-                    found2 = true;
-                    i.second=-1;
-                    j.second=-1;
-                }
-            }
-            if(!found || !found2) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
+    public:
+        bool closeStrings(string word1, string word2) {
+                vector<int>w1(26,0),w2(26,0);
+                        set<char>s1,s2;
+                                for(char c:word1){
+                                            w1[c-'a']++;
+                                                        s1.insert(c);
+                                                                }
+                                                                        for(char c:word2){
+                                                                                    w2[c-'a']++;
+                                                                                                s2.insert(c);
+                                                                                                        }
+                                                                                                                sort(begin(w1),end(w1));
+                                                                                                                        sort(begin(w2),end(w2));
+                                                                                                                                return w1==w2&&s1==s2;
+                                                                                                                                    }
+                                                                                                                                    };
