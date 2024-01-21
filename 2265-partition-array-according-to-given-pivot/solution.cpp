a763e295c5f4eec3d8675777fd8c4879aa4c4fb7
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-01-21 08:06:54 +0000

    [LeetCode Sync] Runtime - 132 ms (77.92%), Memory - 133.7 MB (37.70%)

---
+++ 2265-partition-array-according-to-given-pivot/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> n1;
+        vector<int> n2;
+        int cnt = 0;
+
+        for(auto &i: nums) {
+            if(i == pivot) {
+                cnt++;
+            } else if(i < pivot) {
+                n1.push_back(i);
+            } else {
+                n2.push_back(i);
+            }
+        }
+
+        while(cnt) {
+            n1.push_back(pivot);
+            cnt--;
+        }
+
+        n1.insert(n1.end(), n2.begin(), n2.end());
+
+        return n1;
+    }
+};
