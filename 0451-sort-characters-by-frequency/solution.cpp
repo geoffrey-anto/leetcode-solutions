35547a1a1b49bb7ed53909209eb9e469b01c71bb
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-02-07 07:27:07 +0000

    [LeetCode Sync] Runtime - 123 ms (5.04%), Memory - 11.5 MB (12.10%)

--- 0451-sort-characters-by-frequency/solution.cpp
+++ 0451-sort-characters-by-frequency/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
-
 public:
     string frequencySort(string s) {
-
-    unordered_map<char, int> mp;
-        for (auto &i: s) {
+        unordered_map<char, int> mp;
+        for(auto &i: s) {
             mp[i]++;
         }
 
-        sort(s.begin(), s.end(), [&](char a, char b) {
-        if (mp[a] == mp[b]) {
-            return a < b;
-        } else {
+        sort(begin(s), end(s), [&](auto &a, auto &b) {
+            if(mp[a] == mp[b]) {
+                return a < b;
+            }
+
             return mp[a] > mp[b];
-        }
-    });
+        });
 
         return s;
     }
