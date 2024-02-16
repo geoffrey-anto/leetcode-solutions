a5f815dd43519e3996ed0c7afe2be293a366f276
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-02-16 12:00:14 +0000

    [LeetCode Sync] Runtime - 120 ms (59.26%), Memory - 66.2 MB (53.98%)

---
+++ 1604-least-number-of-unique-integers-after-k-removals/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+
+        for(auto &i: arr) {
+            mp[i]++;
+        }
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(auto &[k, v]: mp) {
+            pq.push(v);
+        }
+
+        mp.clear();
+
+        while(true) {
+            if(pq.top() > k) {
+                break;
+            }
+
+            k = k - pq.top();
+            pq.pop();
+        }
+
+        return pq.size();
+    }
+};
