b5255cff0039295004063cfc85c75d6782d2f99e
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-02-23 19:20:59 +0000

    [LeetCode Sync] Runtime - 32 ms (10.23%), Memory - 29.4 MB (5.03%)

--- 0139-word-break/solution.cpp
+++ 0139-word-break/solution.cpp
@@ -3,13 +3,16 @@ public:
     bool isPresent(vector<string> &x, string str) {
         return find(x.begin(), x.end(), str) != x.end();
     }
+    
     bool solve(int idx,string s,vector<string>& wordDict,int prev,map<pair<int,int>,bool>&dp){
         if(idx>=s.length()) return true;
+
         if(idx==s.length()-1) return isPresent(wordDict, s.substr(prev+1,idx+1));
+
         if(dp.count({idx,prev})>0) return dp[{idx,prev}];
 
         for(int i=idx;i<s.length();i++){
-            string str = s.substr(prev+1,i+1-prev-1);
+            string str = s.substr(prev+1,i-prev);
             if(isPresent(wordDict, str)){
                 return dp[{idx,prev}] = solve(i+1,s,wordDict,i,dp) or solve(i+1,s,wordDict,prev,dp);
             }
