046e6e8abdda8ae817fe3355b2598a45136b3d05
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-02-23 10:42:07 +0000

    [LeetCode Sync] Runtime - 15 ms (72.26%), Memory - 16.4 MB (90.23%)

--- 0803-cheapest-flights-within-k-stops/solution.cpp
+++ 0803-cheapest-flights-within-k-stops/solution.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n, vector<pair<int, int>>());
-
-        for(auto &flight: flights) {
-            adj[flight[0]].push_back(make_pair(flight[1], flight[2]));
-        }   
-
-        queue<pair<int, pair<int, int>>> pq;
-
+    vector<int> dijkstraWithLimit(int src, int n, int lim, vector<vector<pair<int, int>>> &adj) {
         vector<int> dist(n, 1e9);
+
+        //          sz       node d
+        queue<pair<int, pair<int, int>>> q;
 
         dist[src] = 0;
 
-        pq.push({0, {src, 0}});
+        q.push({
+            0, {
+                src, 0
+            }
+        });
 
-        while(!pq.empty()) {
-            auto top = pq.front();
-            pq.pop();
+        while(!q.empty()) {
+            auto top = q.front();
+            q.pop();
 
             int sz = top.first;
             int node = top.second.first;
             int d = top.second.second;
 
-
-            if(sz > k) {
+            if(sz > lim) {
                 continue;
             }
 
-            for(auto &n: adj[node]) {
-                int i = n.first;
-                int w = n.second;
+            for(auto a: adj[node]) {
+                int next = a.first;
+                int w = a.second;
 
-                if(d + w < dist[i] && sz <= k) {
-                    dist[i] = d + w;
-                    pq.push({sz+1, {i, d + w}});
+                if(d + w < dist[next] && sz <= lim) {
+                    dist[next] = d + w;
+
+                    q.push({sz+1, {
+                        next, d + w
+                    }});
                 }
             }
         }
 
-        if(dist[dst] == 1e9) {
-            return -1;
+        return dist;
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+
+        for(auto &f: flights) {
+            adj[f[0]].push_back({
+                f[1], f[2]
+            });
         }
 
-        return dist[dst];
+        auto dist = dijkstraWithLimit(src, n, k, adj);
+
+        return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
