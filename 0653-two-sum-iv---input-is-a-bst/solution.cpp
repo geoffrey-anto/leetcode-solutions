1e1b12ffad54397adce2015c70700aacb906316f
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-24 19:38:16 +0000

    [LeetCode Sync] Runtime - 36 ms (27.99%), Memory - 40.8 MB (8.67%)

--- 0653-two-sum-iv---input-is-a-bst/solution.cpp
+++ 0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -11,10 +11,24 @@
  */
 class Solution {
 public:
+    // bool f(TreeNode* root, int k) {
+    //     if(root->left && root->right) {
+    //         if(k == root->left->val + root->right->val) {
+    //             return true;
+    //         } else if(k < root->left->val + root->right->val) {
+    //             return f(root->right, k);
+    //         } else {
+    //             return f(root->left, k);
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
     unordered_map<int, bool> mp;
 
     bool findTarget(TreeNode* root, int k) {
-        if(root == nullptr) {
+        if(!root) {
             return false;
         }
 
@@ -22,7 +36,7 @@ public:
             return true;
         }
 
-        mp[k-root->val] = true;
+        mp[k - root->val] = true;
 
         return findTarget(root->left, k) or findTarget(root->right, k);
     }
