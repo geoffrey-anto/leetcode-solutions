61110dfe810ccc16ba1b2ac797be2df0d9647ce5
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-25 11:23:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (31.58%)

--- 0088-merge-sorted-array/solution.cpp
+++ 0088-merge-sorted-array/solution.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int last = nums1.size() - 1; // last idx
+        // vector<int> v;
+
+        // int i=0, j=0;
+
+        // while(i < m && j < n) {
+        //     if(nums1[i] <= nums2[j]) {
+        //         v.push_back(nums1[i]);
+        //         i++;
+        //     } else {
+        //         v.push_back(nums2[j]);
+        //         j++;
+        //     }
+        // }
+
+        // while(i < m) {
+        //     v.push_back(nums1[i++]);
+        // }
+
+        // while(j < n) {
+        //     v.push_back(nums2[j++]);
+        // }
+
+        // for(int i=0; i<m; i++) {
+        //     cout << v[i] << endl;
+        //     nums1[i] = v[i];
+        // }
+
+        // return;
+
+        int last = nums1.size() - 1;
         n--;
         m--;
         while(n >= 0) {
@@ -15,5 +44,6 @@ public:
                 last--;
             }
         }
+
     }
 };
