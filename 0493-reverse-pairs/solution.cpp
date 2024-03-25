56d27776bbbd986eeb89513d22e67497646d963f
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-25 11:58:08 +0000

    [LeetCode Sync] Runtime - 403 ms (61.74%), Memory - 102.6 MB (86.39%)

--- 0493-reverse-pairs/solution.cpp
+++ 0493-reverse-pairs/solution.cpp
@@ -1,59 +1,31 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+
+#define ll long long
+#define ordered_set tree<long long, null_type,greater_equal<long long>, rb_tree_tag,tree_order_statistics_node_update> 
+
+
 class Solution {
-public:  
-    void merge(vector<int> &a, int s, int m, int e, int &c) {
-        int l = s;
-        int h = m+1;
-        vector<int> temp;
-        while(l<=m && h<=e) {
-            if(a[l] > a[h]) {
-                temp.push_back(a[h]);
-                h++;
-            } else {
-                temp.push_back(a[l]);
-                l++;
-            }
-        }
-
-        while(l<=m){
-            temp.push_back(a[l]);
-            l++;
-        }
-
-        while(h<=e){
-            temp.push_back(a[h]);
-            h++;
-        }
-
-        for(int i=s; i<=e; i++) {
-            a[i] = temp[i-s];
-        }
-        return;
-    }
-
-    void countInver(vector<int> &a, int s, int m, int e, int &c) {
-        int r = m+1;
-        for(int i=s; i<=m; i++) {
-            while(r <= e && (long long)a[i] > (long long)2*a[r]) {
-                r++;
-            }
-            c += (r - m - 1);
-        }
-    }
-
-    void f(vector<int> &a, int s, int e, int &c) {
-        if(s>=e) {
-            return;
-        }
-        int m = (s + e) / 2;
-        f(a, s, m, c);
-        f(a, m+1, e, c);
-        countInver(a, s, m, e, c);
-        merge(a, s, m, e, c);
-    }
-
+public:
     int reversePairs(vector<int>& nums) {
-        int count = 0;
-        f(nums, 0, nums.size()-1, count);
-        return count;
+        // multiset<int> st;
+        ordered_set st;
+
+        // for(auto i: nums) {
+        //     st.insert(i);
+        // }
+
+        int ans = 0;
+
+        for(auto i: nums) {
+            int cnt = st.order_of_key((ll)i*2);
+
+            ans += cnt;
+
+            st.insert(i);
+        }
+
+        return ans;
     }
 };
