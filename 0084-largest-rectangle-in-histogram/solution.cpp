d7e2e4bca22a6ef857206603027305b418a6a313
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-25 16:41:32 +0000

    [LeetCode Sync] Runtime - 116 ms (49.81%), Memory - 84 MB (45.15%)

--- 0084-largest-rectangle-in-histogram/solution.cpp
+++ 0084-largest-rectangle-in-histogram/solution.cpp
@@ -4,39 +4,31 @@ public:
         const int n = arr.size();
         stack<int> st;
 
-        st.push(-1);
-        vector<int> nextMin(n);
+        vector<int> nextMin(n, -1);
         for(int i=n-1; i>=0; i--) {
-            while(st.top() != -1 and arr[st.top()] >= arr[i]) {
+            while(!st.empty() and arr[st.top()] >= arr[i]) {
                 st.pop();
             }
 
             if(!st.empty()) {
                 nextMin[i] = st.top();
-                st.push(i);
-            } else {
-                nextMin[i] = arr[i];
-                st.push(arr[i]);
-            }
+            } 
+
+            st.push(i);
 
         }
 
         st = stack<int>();
 
-        st.push(-1);
-        vector<int> prevLess(n);
+        vector<int> prevLess(n, -1);
 
         for(int i=0; i<n; i++) {
-            while(st.top() != -1 and arr[st.top()] >= arr[i]) {
+            while(!st.empty() and arr[st.top()] >= arr[i]) {
                 st.pop();
             }
 
             if(!st.empty()) {
                 prevLess[i] = st.top();
-                st.push(i);
-            } else {
-                prevLess[i] = arr[i];
-                st.push(arr[i]);
             }
 
             st.push(i);
@@ -54,8 +46,7 @@ public:
             mx = max(mx, x * curr); 
         }
 
-        
-
+    
         return mx;
     }
 };
