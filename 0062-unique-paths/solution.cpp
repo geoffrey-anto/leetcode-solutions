1c857962d1b22ead9209399c428b2d1f0dc933d7
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-25 12:04:29 +0000

    [LeetCode Sync] Runtime - 3 ms (33.16%), Memory - 7.7 MB (17.02%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -33,8 +33,19 @@ public:
 
         return dp[x][y] = f(x+1, y, m, n, dp) + f(x, y+1, m, n, dp);
     }
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(0, 0, m, n, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        dp[0][0] = 1;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i > 0) dp[i][j] += dp[i-1][j];
+                if(j > 0) dp[i][j] += dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
