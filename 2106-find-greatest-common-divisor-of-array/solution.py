8559e43cdc26f506dabc7d2138bca6859fd947f5
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2024-03-27 23:16:38 +0530

    fixed fkup

--- 2106-find-greatest-common-divisor-of-array/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findGCD(self, nums: List[int]) -> int:
-        min_=min(nums)
-        max_=max(nums)
-        GCD=0
-        for i in range(1,min_+1):
-            if min_%i==0 and max_%i==0:
-                GCD=i
-        return GCD
