ed8a0f03890973f559015188f4688908ef9e4360
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-03-27 19:50:44 +0000

    [LeetCode Sync] Runtime - 21 ms (5.37%), Memory - 23.2 MB (29.93%)

--- 0042-trapping-rain-water/solution.cpp
+++ 0042-trapping-rain-water/solution.cpp
@@ -11,23 +11,18 @@ public:
         int ans = 0;
         for(int i=0; i<n;i++) {
             while(!st.empty() && height[i] > height[st.top()]) {
-                // right is the prev point
                 int rightMaxIdx = st.top();
                 st.pop();
 
-                // check if there is a left idx
                 if (st.empty()) {
                     break;
                 }
 
                 int leftMaxIdx = st.top();
 
-                // either take the curr height or the left one whichever is MIN
                 int x = min(height[i], height[leftMaxIdx]) - height[rightMaxIdx];
-                // find the width
                 int y = i - leftMaxIdx - 1;
 
-                // accumulate answer
                 ans += (x * y);
             }
 
