993c34eddbecfdcfa1665d31cda19167b80650d0
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-04-02 13:44:27 +0000

    [LeetCode Sync] Runtime - 8 ms (35.06%), Memory - 9.5 MB (14.49%)

--- 0205-isomorphic-strings/solution.cpp
+++ 0205-isomorphic-strings/solution.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        
-            int n=s.length();
-            unordered_map<char, char> s_map; 
-            unordered_map<char, char> t_map;
+        unordered_map<char, char> mp;
+        unordered_map<char, bool> vis;
 
-        for (int i = 0; i < n; i++) 
-        {
-            if (s_map.find(s[i]) == s_map.end() && t_map.find(t[i]) == t_map.end()) 
-            {
-                s_map[s[i]] = t[i];
-                t_map[t[i]] = s[i];
-            } 
-            else {
-    
-                if (s_map[s[i]] != t[i] || t_map[t[i]] != s[i])           {
+        for(int i=0; i<s.size(); i++) {
+            if(mp.find(t[i]) == mp.end() && !vis[s[i]]) {
+                mp[t[i]] = s[i];
+                vis[s[i]] = true;
+            } else {
+                if(mp[t[i]] != s[i]) {
                     return false;
+                } else {
+                    continue;
                 }
             }
         }
+
         return true;
     }
 };
