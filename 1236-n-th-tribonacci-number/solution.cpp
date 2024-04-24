655e8a3762199a3f68abf0833b42774d506dcc71
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-04-24 04:47:35 +0000

    [LeetCode Sync] Runtime - 2 ms (51.22%), Memory - 7.2 MB (27.28%)

--- 1236-n-th-tribonacci-number/solution.cpp
+++ 1236-n-th-tribonacci-number/solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int dp[1000];
-    int helper(int n){
-        if(n==2) return 1;
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=helper(n-1)+helper(n-2)+helper(n-3); 
-    }
-    
     int tribonacci(int n) {
-        memset(&dp,-1,sizeof(dp));
-        return helper(n);
+        if(n == 0) {
+            return 0;
+        }
+        
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 1;
+
+        for(int i=2; i<n; i++) {
+            int temp = n1 + n2 + n3;
+
+            n1 = n2;
+            n2 = n3;
+            n3 = temp;
+        }
+
+        return n3;
     }
 };
