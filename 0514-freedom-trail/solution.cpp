9afa98d16f834395927d6329804d1b7ce58d9b51
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-04-27 07:21:41 +0000

    [LeetCode Sync] Runtime - 22 ms (43.48%), Memory - 13.8 MB (74.50%)

--- 0514-freedom-trail/solution.cpp
+++ 0514-freedom-trail/solution.cpp
@@ -1,75 +1,35 @@
 class Solution {
 public:
-    int dp[101][101];
-
-    int f(int pos, int k, string &ring, string &key) {
-
+    int f(int pos, int k, string &ring, string &key, int n, vector<vector<int>> &dp) {
         if(k == key.size()) {
             return 0;
         }
 
-        int ans = 1e6;
+        if(dp[pos][k] != -1) return dp[pos][k];
 
-        int n = ring.size();
+        int res = INT_MAX;
 
-        if(dp[pos][k] != -1 and dp[pos][k] != 0) return dp[pos][k];
+        int i = pos;
 
-        // if(ring[pos] == key[0]) {
-        //     ans += 1 + f(pos, ring, key.substr(1));
-        // } else {
-            // take pos of key[0] from left ans right and recursively procced to take min of the 2
+        for(int lim = 0; lim < n; lim++) {
+            int iC = i % n;
 
-            // int l = pos; int r = pos;
+            if(ring[iC] == key[k]) {
+                int cost = min(lim, n-lim);
 
-            // int cnt1 = 0;
-            // int cnt2 = 0;
+                int ans = cost + 1 + f(iC, k+1, ring, key, n, dp);
 
-            // while(true) {
-            //     if(ring[l] == key[0]) {
-            //         break;
-            //     }
-            //     cnt1++;
-
-            //     l--;
-            //     if(l < 0) {
-            //         l = ring.size()-1;
-            //     }
-            // }
-
-            // while(true) {
-            //     if(ring[r] == key[0]) {
-            //         break;
-            //     }
-            //     cnt2++;
-
-            //     r++;
-            //     if(r == ring.size()) {
-            //         r = 0;
-            //     }
-            // }
-
-            // ans += 1 + min((cnt1 + f(l, ring, key.substr(1))), (cnt2 + f(r, ring, key.substr(1))));
-
-            // printf("in: %d, need %c, cnt1: %d, cnt2: %d  -> took: %d\n", pos, key[0], cnt1, cnt2, ans);
-
-            int lim = 0;
-
-            // try out all the possible values of the same character to get the best via dp
-            for(int i=pos; lim < n; i++, lim++) {
-                i %= n;
-
-                if(ring[i] == key[k]) {
-                    int cost = min(lim, n - lim) + 1;
-                    int v = cost + f(i, k+1, ring, key);
-                    ans = min(ans, v);
-                }
+                res = min(ans, res);
             }
-        // }
 
-        return dp[pos][k] = ans;
+            i++;
+        }
+
+        return dp[pos][k] = res;
     }
+
     int findRotateSteps(string ring, string key) {
-        memset(dp, -1, sizeof(dp));
-        return f(0, 0, ring, key);
+        vector<vector<int>> dp(ring.size()+1, vector<int>(key.size()+1, -1));
+        return f(0, 0, ring, key, ring.size(), dp);
     }
 };
