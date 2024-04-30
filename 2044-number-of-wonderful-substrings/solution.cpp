d075df7aac8cc3974b612c9598ef7b6e9d9c19ad
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-04-30 09:25:12 +0000

    [LeetCode Sync] Runtime - 367 ms (32.43%), Memory - 18.7 MB (38.34%)

---
+++ 2044-number-of-wonderful-substrings/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        int runningMask = 0;
+        unordered_map<int, int> mp; mp[0] = 1;
+        long long ans = 0;
+
+        for(auto i: word) {
+            runningMask = runningMask ^ (1 << (i-'a'));
+
+            if(mp.count(runningMask) > 0) {
+                ans += (long long)(mp[runningMask]);
+                mp[runningMask]++;
+            } else {
+                mp[runningMask] = 1;
+            }
+
+            for(int j=0; j<11; j++) {
+                int m = runningMask ^ (1 << j);
+                if(mp.count(m) > 0) {
+                    ans += mp[m];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
