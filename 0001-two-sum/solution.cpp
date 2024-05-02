67249d0ad956a6792ec924fcfa4f95909218336a
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-05-02 15:41:00 +0000

    [LeetCode Sync] Runtime - 20 ms (48.53%), Memory - 12.9 MB (52.26%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-
-        int n = nums.size();
-
-        for(int i=0; i<n; i++)
-        {
-            if(mp.find(nums[i]) != mp.end()) {
-                return {mp[nums[i]], i};
-            } else {
-                mp[target-nums[i]] = i;
+      	vector<int> idxMap(nums.size(), 0);
+      
+    	for(int i=0; i<nums.size(); i++) {
+          idxMap[i] = i;
+        }
+      	
+        sort(idxMap.begin(),idxMap.end(), [&](auto &a, auto &b) {
+          if(nums[a] < nums[b]) {
+            	return true;
+          }
+          return false;
+        });
+      	
+        int left=0;
+        int right=nums.size()-1;
+      
+        while(left<right){
+            if(nums[idxMap[left]]+nums[idxMap[right]]==target){
+                return {idxMap[left],idxMap[right]};
             }
-        }      
+            else if(nums[idxMap[left]]+nums[idxMap[right]]<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+      
         return {};
     }
 };
