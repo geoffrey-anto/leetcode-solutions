aeb9c0e36e3756dfe2771d3938a70a6511247158
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-05-21 08:37:05 +0000

    [LeetCode Sync] Runtime - 5 ms (13.02%), Memory - 8.4 MB (46.19%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -3,15 +3,13 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
 
-        int p = 1 << n;
-
         vector<vector<int>> ans;
 
-        for(int i=0; i<p; i++) {
+        for(int mask=0; mask<(1 << n); mask++) {
             vector<int> t;
 
             for(int j=0; j<n; j++) {
-                if(i & (1 << j)) {
+                if(mask & (1 << j)) {
                     t.push_back(nums[j]);
                 }
             }
