9d79cd5b81d615d4028bd991555e668b3176a5ce
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-06-07 10:42:52 +0000

    [LeetCode Sync] Runtime - 80 ms (50.32%), Memory - 76.8 MB (55.79%)

---
+++ 0648-replace-words/solution.cpp
@@ -0,0 +1,172 @@
+class Node {
+    private:
+    int n = 26;
+
+    public:
+    Node* ref[26];
+    int prefix;
+    int end;
+
+    Node() {
+        memset(ref, 0, sizeof(ref));
+        prefix = 0;
+        end = 0;
+    }
+
+    bool contains(char a) {
+        return (ref[a - 'a'] != nullptr);
+    }
+
+    void set(char a, Node* node) {
+        ref[a - 'a'] = node;
+    }
+
+    Node* get(char a) {
+        return ref[a - 'a'];
+    } 
+
+    void setPrefix() {
+        prefix++;
+    }
+
+    void setEnd() {
+        end++;
+    }
+
+    void eraseEnd() {
+        end--;
+    }
+
+    void erasePrefix() {
+        prefix--;
+    }
+
+    int getEnd() {
+        return end;
+    }
+
+    int getPrefix() {
+        return prefix;
+    }
+};
+
+class Trie {
+    public:
+    Node* root;
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string &s) {
+        Node* temp = root;
+
+        for(auto &ch: s) {
+            if(!temp->contains(ch)) {
+                Node* node = new Node();
+                temp->set(ch, node);
+            }
+            temp = temp->get(ch);
+            temp->setPrefix();
+        }
+        temp->setEnd();
+    }
+
+    int countWordsEqualTo(string &s) {
+        Node* temp = root;
+
+        for(auto &ch: s) {
+            if(!temp->contains(ch)) {
+                return 0;
+            }
+            temp = temp->get(ch);
+        }
+
+        return temp->getEnd();
+    }
+
+    string getSmallestPrefix(string &s) {
+        Node* temp = root;
+
+        string res = "";
+
+        for(auto &ch: s) {
+            if(temp->getEnd()) {
+                return res;
+            }
+
+            if(!temp->contains(ch)) {
+                return s;
+            }
+
+            temp = temp->get(ch);
+
+            res += ch;
+        }
+
+        return res;
+    }
+
+    int countWordsStartingWith(string &s) {
+        Node* temp = root;
+
+        for(auto &ch: s) {
+            if(!temp->contains(ch)) {
+                return 0;
+            }
+            temp = temp->get(ch);
+        }
+
+        return temp->getPrefix();
+    }
+
+    bool isPrefix(string &s) {
+        Node* temp = root;
+
+        for(auto &ch: s) {
+            if(!temp->contains(ch)) {
+                return false;
+            }
+            temp = temp->get(ch);
+        }
+
+        return true;
+    }
+
+    void erase(string &s) {
+        Node* temp = root;
+
+        for(auto &ch: s) {
+            if(!temp->contains(ch)) {
+                return;
+            }
+
+            temp = temp->get(ch);
+            temp->erasePrefix();
+        }
+
+        temp->eraseEnd();
+    }
+};
+
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string &sentence) {
+        stringstream ss(sentence);
+
+        string ans = "";
+
+        Trie* t = new Trie();
+
+        for(auto &i: dictionary) {
+            t->insert(i);
+        }
+
+        string temp;
+        while(getline(ss, temp, ' ')) {
+            ans += t->getSmallestPrefix(temp) + " ";
+        }
+
+        return ans.substr(0, ans.size() - 1);
+    }
+};
