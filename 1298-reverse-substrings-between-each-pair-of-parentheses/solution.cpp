5bbb84c4872515e0e9ed937a9f22ec8d7eb7f64a
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-07-11 09:20:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (100.00%)

--- 1298-reverse-substrings-between-each-pair-of-parentheses/solution.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/solution.cpp
@@ -1,64 +1,27 @@
 class Solution {
 public:
-    // string reverseParentheses(string s) {
-    //     int l = 0;
+    int i = 0;
+    string helper(string& s) {
+        string result;
 
-    //     int n = s.size();
-
-    //     int r = n - 1;
-
-    //     while(l <= r) {
-    //         while(l < n and s[l] != '(') {
-    //             l++;
-    //         }
-
-    //         while(r >= 0 and s[r] != ')') {
-    //             r--;
-    //         }
-
-    //         reverse(s.begin()+l, s.begin()+r+1);
-    //         for(int i=l; i<r+1; i++) {
-    //             if(s[i] == '(') {
-    //                 s[i] = ')';
-    //             } else if(s[i] == ')') {
-    //                 s[i] = '(';
-    //             }
-    //         }
-    //         l++;
-    //         r--;
-    //     }
-
-    //     string ans = "";
-
-    //     for(int i=0; i<n; i++) {
-    //         if(s[i] != ')' and s[i] != '(') {
-    //             ans += s[i];
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-
-    string reverseParentheses(string s) {
-        int n = s.length();
-        vector<int> opened, pair(n);
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '(')
-                opened.push_back(i);
+        while (i < s.length()) {
             if (s[i] == ')') {
-                int j = opened.back();
-                opened.pop_back();
-                pair[i] = j;
-                pair[j] = i;
+                i++;
+                reverse(result.begin(), result.end());
+                return result;
+            } else if (s[i] == '(') {
+                i++;
+                string st = helper(s);
+                result += st;
+            } else {
+                result += s[i];
+                i++;
             }
         }
-        string res;
-        for (int i = 0, d = 1; i < n; i += d) {
-            if (s[i] == '(' || s[i] == ')')
-                i = pair[i], d = -d;
-            else
-                res += s[i];
-        }
-        return res;
+        return result;
+    }
+
+    string reverseParentheses(string s) {
+        return helper(s);
     }
 };
