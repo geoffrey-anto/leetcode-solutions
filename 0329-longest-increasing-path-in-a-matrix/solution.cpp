82bdda583b68eb7e188de74ab7bec30d3d79f563
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-07-23 17:31:10 +0000

    [LeetCode Sync] Runtime - 30 ms (27.31%), Memory - 19.4 MB (95.67%)

--- 0329-longest-increasing-path-in-a-matrix/solution.cpp
+++ 0329-longest-increasing-path-in-a-matrix/solution.cpp
@@ -19,6 +19,23 @@ public:
         return dp[i][j] = ans;
     }
 
+    string path(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        if(dp[i][j] == 0) {
+            return to_string(matrix[i][j]);
+        }
+
+        for(int p=0; p<4; p++) {
+            int dx = d[p] + i;
+            int dy = d[p+1] + j;
+
+            if(dx >= 0 and dx < matrix.size() and dy >= 0 and dy < matrix[0].size() && (dp[i][j] == dp[dx][dy] + 1)) {
+               return to_string(matrix[i][j]) + path(dx, dy, matrix, dp);
+            }
+        }
+
+        return "";
+    }
+
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
         int ans = 0;
@@ -27,6 +44,25 @@ public:
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 ans = max(ans, dfs(i, j, matrix, dp) + 1);
+            }
+        }
+
+        bool isFound = false;
+
+        // Trace Path
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(dp[i][j] == ans - 1) {
+                    // cout << path(i, j, matrix, dp) << endl;
+                    isFound = true;
+
+                    if(isFound) {
+                        break;
+                    }
+                }
+                if(isFound) {
+                    break;
+                }
             }
         }
 
