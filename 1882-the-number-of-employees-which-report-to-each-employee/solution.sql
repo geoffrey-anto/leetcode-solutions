fbecadc7013c8dcea6af555c61744f9aa2045a82
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-08-03 12:13:42 +0000

    [LeetCode Sync] Runtime - 659 ms (72.10%), Memory - 0.0B (100.00%)

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT e2.employee_id, e2.name, COUNT(e1.employee_id) as reports_count, ROUND(AVG(e1.age)) as average_age FROM Employees as e1 INNER JOIN Employees as e2 ON e1.reports_to=e2.employee_id GROUP BY e1.reports_to ORDER BY e2.employee_id;
