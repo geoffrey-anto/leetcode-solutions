1040c2dd0c878634d8a4922e9f311c9e89a6e78b
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-08-06 12:46:42 +0000

    [LeetCode Sync] Runtime - 333 ms (87.44%), Memory - 0.0B (100.00%)

---
+++ 2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT employee_id FROM Employees WHERE manager_id NOT IN (SELECT DISTINCT(employee_id) FROM Employees) AND salary < 30000 ORDER BY employee_id;
+
