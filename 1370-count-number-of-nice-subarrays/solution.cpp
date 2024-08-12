dcf47390101ae6a82ce742114087faf16046262b
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-08-12 14:17:30 +0000

    [LeetCode Sync] Runtime - 105 ms (6.72%), Memory - 70.3 MB (100.00%)

--- 1370-count-number-of-nice-subarrays/solution.cpp
+++ 1370-count-number-of-nice-subarrays/solution.cpp
@@ -1,55 +1,21 @@
 class Solution {
 public:
-    bool isOdd(int n) {
-        return (n % 2) != 0;
+    int atmost(int k, vector<int> &nums) {
+        int numsSize = nums.size();
+        int i, j=0, count=0, odds=0;
+        for (i = 0; i < numsSize; ++i) {
+            odds += (nums[i] & 1);
+            while(odds == k) {
+                count += numsSize - i;
+                odds -= (nums[j++] & 1);
+            }
+        }
+        return count;
     }
+
+
+
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int prev = 0;
-        int curr = 0;
-        int mx1 = 0;
-        int cnt = 0;
-
-        int n = nums.size();
-        
-        for(curr = 0; curr < n; curr++) {
-            if(isOdd(nums[curr])) {
-                cnt++;
-            }
-
-            while (cnt > k) {
-                if(isOdd(nums[prev])) {
-                    cnt--;
-                }
-                prev++;
-            }
-
-            mx1 += (curr - prev + 1);
-        }
-
-        if(k==0) {
-            return mx1;
-        }
-
-        prev = 0;
-        curr = 0;
-        int mx2 = 0;
-        cnt = 0;
-        
-        for(curr = 0; curr < n; curr++) {
-            if(isOdd(nums[curr])) {
-                cnt++;
-            }
-
-            while (cnt > (k-1)) {
-                if(isOdd(nums[prev])) {
-                    cnt--;
-                }
-                prev++;
-            }
-
-            mx2 += (curr - prev + 1);
-        }
-
-        return mx1 - mx2;
+        return atmost(k, nums) - atmost(k+1, nums);      
     }
 };
