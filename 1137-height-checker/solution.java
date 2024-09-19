eb021959d816919b4dc3144613b6ebf39d91016c
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-09-19 08:40:19 +0000

    [LeetCode Sync] Runtime - 2 ms (89.17%), Memory - 41.5 MB (48.16%)

---
+++ 1137-height-checker/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int cnt=0;
+        int arr[]=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            arr[i]=heights[i];
+        }
+        Arrays.sort(heights);
+        
+        
+        for(int i=0;i<heights.length;i++){
+            if(arr[i]!=heights[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+}
