92fc22a6ddc3efcfb7117975d3d33b5d801c10b4
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-10-18 18:31:16 +0000

    [LeetCode Sync] Runtime - 82 ms (23.07%), Memory - 10.2 MB (100.00%)

--- 2170-count-number-of-maximum-bitwise-or-subsets/solution.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/solution.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-
     int countMaxOrSubsets(vector<int>& nums) {
-        int x = nums[0];
-        for(int i=1; i<nums.size(); i++) {
-            x = x | nums[i];
+        int mx = 0;
+
+        for(auto i: nums) {
+            mx |= i;
         }
 
-        long long ans = 0ll;
+        int ans = 0;
 
         for(int i=0; i<pow(2, nums.size()); i++) {
             int curr = 0;
+
             for(int j=0; j<nums.size(); j++) {
-                if((i & (1 << j)) != 0) {
-                    curr = curr | nums[j];
+                if(i & (1 << j)) {
+                    curr |= nums[j];
                 }
             }
-            
-            if(x == curr) ans++;
+
+            if(curr == mx) {
+                ans++;
+            }
         }
 
         return ans;
