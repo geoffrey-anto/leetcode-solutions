fb32bf894a6497a31b422083226bd568045ed98e
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2024-12-22 21:10:34 +0000

    [LeetCode Sync] Runtime - 49 ms (92.53%), Memory - 248.2 MB (100.00%)

--- 3181-find-building-where-alice-and-bob-can-meet/solution.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/solution.cpp
@@ -1,94 +1,46 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& heights) {
-        stack<int> st;
-        set<int> arr;
-        int n = heights.size();
-
-        vector<int> nextGreater(n, -1);
-
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && heights[st.top()] <= heights[i]) {
-                st.pop();
-            }
-
-            if(!st.empty()) {
-                arr.insert(st.top());
-                nextGreater[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        // for(auto i:arr) {
-        //     cout << i << endl;
-        // }
-
-        cout<< endl;
-
-        // for(auto i: nextGreater){
-        //     cout << i << endl;
-        // }
-
-        return nextGreater;
-    }
+    using int2 = pair<int, int>;
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
-        //  0  1  2  3  4  5
-        // [6, 4, 8, 5, 2, 7];
-        // [2, 2, -1, 5, 5, -1] // take this for Q and find the next greatest element
-        // [0,1]
-        // [0,3]
-        // [2,4]
-        // [3,4]
-        // [2,2]
+        int n=heights.size(), qz=queries.size();
+        vector<int> ans(qz, -1);
+        vector<int2> idx;
 
-                int n = heights.size();
-
-                vector<int> ans;
-
-        auto nextGreater = nextGreaterElements(heights);
-
-        for(auto q: queries) {
-            int a = q[0];
-            int b = q[1];
-            if(a == b) {
-                ans.push_back(a);
-                continue;
-            }
-
-            if(a > b) {
-                swap(a,b);
-            }
-            int xa = nextGreater[a];
-            int xb = nextGreater[b];
-
-            if(heights[a] < heights[b]) {
-                ans.push_back(b);
-                continue;
-            }
-
-
-            if(xa == -1 or xb == -1) {
-                ans.push_back(-1);
-                continue;
-            }
-
-            // if() {
-            //     ans.push_back(xb);
-            //     continue;
-            // }
-
-
-            while ( xb >= 0 && xb < n && heights[xb] <= heights[a] ) {
-                xb = nextGreater[xb];
-            }
-
-
-        ans.push_back(xb);
-
-
+        for (int i= 0; i< qz; i++) {
+            int& x=queries[i][0], & y=queries[i][1];
+            if (x > y) // let x <= y
+                swap(x, y);
+            if (x == y|| heights[x]<heights[y])
+                ans[i] = y;
+            else idx.emplace_back(y, i);
         }
 
+        sort(idx.begin(), idx.end(), greater<>());
+        vector<int2> stack;
+
+        int j=n-1;
+        for (auto [_, i] : idx) {
+            int x = queries[i][0];
+            int y = queries[i][1];
+            for (; j >y; j--) {
+                while (!stack.empty() && heights[stack.back().second] < heights[j])
+                    stack.pop_back();
+                stack.emplace_back(heights[j], j);
+            }
+
+            // Check if accessing elements beyond the bounds of stack here
+            auto it=upper_bound(stack.rbegin(), stack.rend(), make_pair(heights[x], n));
+            ans[i]=(it==stack.rend()) ?-1 : it->second;
+        }
         return ans;
     }
 };
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
