a79ce42eed1fe4b7fdeaa2526fe35c8e559d8a43
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2025-03-31 18:33:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.7 MB (70.98%)

---
+++ 3832-minimum-cost-to-reach-every-position/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+        int n = cost.size();
+
+        vector<int> ans(n);
+
+        ans[0] = cost[0];
+
+        for(int i=1; i<n; i++) {
+            ans[i] = min(ans[i-1], cost[i]);
+        }
+
+        return ans;
+    }
+};
