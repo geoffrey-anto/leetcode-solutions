2af03fccb15e4f2c5e740975c2f3d9e9f6db2321
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2025-04-14 06:14:55 +0000

    [LeetCode Sync] Runtime - 14 ms (7.97%), Memory - 4 MB (96.12%)

---
+++ 1656-count-good-triplets/solution.go
@@ -0,0 +1,19 @@
+import "math"
+
+func countGoodTriplets(arr []int, a int, b int, c int) int {
+    ans := 0
+
+    n := len(arr)
+
+    for i := 0; i < n; i++ {
+        for j := i+1; j < n; j++ {
+            for k := j+1; k<n; k++ {
+                if (math.Abs(float64(arr[i] - arr[j])) <= float64(a)) && (math.Abs(float64(arr[j] - arr[k])) <= float64(b)) && (math.Abs(float64(arr[i] - arr[k])) <= float64(c)) {
+                    ans = ans + 1;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
